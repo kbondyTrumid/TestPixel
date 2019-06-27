5d73e4e86a7a94fa224792899d6e437c28ef95cd
Kieran Bondy <kieranbondy@Trumids-MBP.ad.trumid.com>
2019-06-27 15:43:54 -0400

    y

--- PixTracker.php
+++ PixTracker.php
@@ -1,33 +1,3 @@
 <?php
-header("Content-Type: image/jpeg"); // it will return image 
-readfile("img.jpg");
-
-    function get_client_ip() {
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if(isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-    }
-
-$PublicIP = get_client_ip();
-$time = new DateTime("now", new DateTimeZone("America/New_York"));
-$file = '../OutputText.txt';
-$current = file_get_contents($file);
-$current .= $time->format('m/d/Y, H:i:s');
-$current .= " ";
-$current .= $PublicIP;
-$current .= "\n";
-file_put_contents($file, $current);
+echo "Hello"
 ?>
